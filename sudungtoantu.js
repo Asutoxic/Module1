e7344fd73907a7624a43b6c4bfac65ade3b23bf3
Asutoxic <trankiemgiavu.tkgv@gmail.com>
2022-05-11 14:23:06 +0700

    add sudungtoantu

---
+++ sudungtoantu.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputHeight = prompt('Nhập vào chiều cao');
+inputWidth = prompt('Nhập vào chiều rộng');
+let width = parseInt(inputWidth);
+let heigh = parseInt(inputHeight);
+let Area = width * heigh;
+document.write('Diện tích hình chữ nhật là:' + Area)
